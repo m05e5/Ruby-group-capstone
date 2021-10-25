ffd86368335f978c94b8f1be001c1ed6282f4235
Sylvestre <pasytchangwa@gmail.com>
2021-10-25 16:02:57 +0300

    Add add_genre and add_author methods in Item class

--- item.rb
+++ item.rb
@@ -8,6 +8,15 @@ class Item
     @archived = false
   end
 
+  def add_genre(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items.push(self) unless genre.items.include?(self)
+  end
 
   private
 
