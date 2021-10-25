f709baf9414d3a5bfd7de0138f7ebc3f730e688a
Sylvestre <pasytchangwa@gmail.com>
2021-10-25 22:32:08 +0300

    Add test case for genre file

---
+++ spec/genre_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'required_files'
+
+describe Genre do
+  before :each do
+    @genre = Genre.new 'programming'
+  end
+
+  describe '#add_item' do
+    it 'should add an item' do
+      item = Item.new Time.now
+      @genre.add_item item
+      expect(@genre.items).to include(item)
+    end
+  end
+end
