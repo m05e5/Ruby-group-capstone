3b265308c766f6c8e9d8212de310845a1de5cbde
Sylvestre <pasytchangwa@gmail.com>
2021-10-25 19:01:44 +0300

    Create genre.rb file and create add_item method

---
+++ genre.rb
@@ -0,0 +1,22 @@
+class Genre
+  attr_accessor :name, :id
+  attr_reader :items
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name: = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.add_genre self
+  end
+
+  def to_s
+    @name
+  end
+
+end
+
+
