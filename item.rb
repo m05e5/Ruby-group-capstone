0b8a1bf4703af76789720e34b8317eac2c599a53
sneeu-leeu <sa070197@gmail.com>
2021-10-25 14:56:05 +0200

    Add private can be archived method

--- item.rb
+++ item.rb
@@ -1,3 +1,17 @@
 class Item
-  
+  attr_reader :genre, :author, :source, :label, :archived
+  attr_accessor :id
+
+  def initialize(publish_date)
+    @id = Random(1...1000)
+    @publish_date = publish_date
+    @archived = false
+  end
+
+
+  private
+
+  def can_be_archived
+    (Time.now.year - @publish_date.year) > 10
+  end
 end
