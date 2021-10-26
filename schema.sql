44470d64ac104e406b9c93beb8a4fa03795eb928
m05e5 <calebmoses1111@gmail.com>
2021-10-26 18:42:29 +0100

    add sql tables

--- schema.sql
+++ schema.sql
@@ -18,3 +18,24 @@ CREATE TABLE music_album (
   FOREIGN KEY(genre_id) REFERENCES genre(id),
   FOREIGN KEY(author_id) REFERENCES author(id),  FOREIGN KEY(label_id) REFERENCES label(id)
 );
+
+CREATE TABLE label (
+  id    INT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(100),
+  color VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE music_album (
+  id           INT GENERATED ALWAYS AS IDENTITY,
+  publisher    VARCHAR(100),
+  cover_state  VARCHAR(100),
+  publish_date DATE,
+  genre_id     INT,
+  author_id    INT,
+  label_id     INT,
+  archived     BOOLEAN,
+  PRIMARY KEY(id),
+  FOREIGN KEY(genre_id) REFERENCES genre(id),
+  FOREIGN KEY(author_id) REFERENCES author(id),  FOREIGN KEY(label_id) REFERENCES label(id)
+);
