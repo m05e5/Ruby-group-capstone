3521a0be8737b2bf46f28d5ec535669fe22a94df
sneeu-leeu <sa070197@gmail.com>
2021-10-27 14:34:48 +0200

    create db for authors and games

--- schema.sql
+++ schema.sql
@@ -40,3 +40,27 @@ CREATE TABLE book (
   FOREIGN KEY(author_id) REFERENCES author(id),  
   FOREIGN KEY(label_id) REFERENCES label(id)
 );
+
+CREATE TABLE games (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    multiplayer BOOLEAN,
+    last_played_at DATE,
+    genre_id INT,
+    author_id INT,
+    source_id INT,
+    label_id INT,
+    publish_date: DATE,
+    archived: BOOLEAN,
+    PRIMARY KEY(id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (source_id) REFERENCES sources (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
+)
+
+CREATE TABLE authors (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    PRIMARY KEY(id)
+)
